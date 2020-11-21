c633bb9bee8ba1911dab91a953b5ca942dcb28c1
Delvaux <Robby>
2020-11-21 13:53:26 +0100

    remove env from gitignore

--- BecorumStart/public/js/app.js
+++ BecorumStart/public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\Becorum\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\Becorum\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\Dualvex\Desktop\testttt\Workshop\BecorumStart\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Dualvex\Desktop\testttt\Workshop\BecorumStart\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
